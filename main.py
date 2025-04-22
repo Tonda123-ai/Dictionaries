67d9dd9563332df07e40c33efdf055a5b7116e3f
Tonda123-ai <antonin.burianek@infis.cz>
2025-04-22 09:33:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-
-robots = {"robot1": "údržba", "robot2": "úklid", "robot3": "vaření"}
+robots = {"Anna": "údržba", "Jakub": "úklid", "Filip": "vaření", "Eva": "nákupy", "Petr": "zahradničení"} 
 
 while True:
-    name = input("Zadejte jméno robota (nebo 'konec' pro ukončení): ").strip()
+    name = input("Zadejte jméno robota (nebo 'konec' pro ukončení): ")
     
-    if name.lower() == "konec":
+    if name == "konec":
         break 
     if name in robots:
-
         print(f"Funkce robota {name}: {robots[name]}")
     else:
-        function = input(f"Robot {name} není v seznamu. Zadejte jeho funkci: ").strip()
+        function = input(f"Robot {name} není v seznamu. Zadejte jeho funkci: ")
         robots[name] = function
         print(f"Robot {name} byl přidán s funkcí: {function}")
 print("\nSeznam robotů a jejich funkcí:")
